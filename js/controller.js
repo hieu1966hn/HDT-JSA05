a7e9f0d27123cc363b57985d7644c4d12d41f3bf
hieu1966hn <hieu1966hn@gmail.com>
2021-03-21 10:51:47 +0700

    change controller

--- js/controller.js
+++ js/controller.js
@@ -7,14 +7,37 @@ controller.register = (data) => {
         document.getElementById("first-name-error").innerText = "";
     }
 
-    if(data.lastName ===""){
+    if (data.lastName === "") {
         document.getElementById('last-name-error').innerText = 'please input last name'
     }
-    else{
+    else {
         document.getElementById('last-name-error').innerText = ''
     }
 
-    if(data.email ===""){
-        document.getElementById('email-error').innerText="please input email"
+    if (data.email === "") {
+        document.getElementById('email-error').innerText = "please input email"
     }
+    else {
+        document.getElementById('email-error').innerText = ""
+    }
+
+    if (data.password === "") {
+        document.getElementById('password-error').innerText = "please input password"
+    }
+    else {
+        document.getElementById('password-error').innerText = ""
+    }
+
+    if (data.confirmPassword === "") {
+        document.getElementById('confirm-password-error').innerText = "please input confirm password"
+    }
+    else if(data.confirmPassword.trim() !== data.password.trim()){
+        document.getElementById('confirm-password-error').innerText = "Password does not match"
+    }
+    else{
+        document.getElementById('confirm-password-error').innerText = ""
+    }
+
+
+    
 }
